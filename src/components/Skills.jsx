420738995be9a401b71526731396f35cbdb1b9ed
tjsingh <taranjeets120@gmail.com>
2022-10-03 16:18:20 +0530

    work component is completed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,10 +5,10 @@ import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
 import FireBase from '../assets/firebase.png';
-import AWS from '../assets/aws.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png';
+// import AWS from '../assets/aws.png';
+// import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
     return (
@@ -17,7 +17,7 @@ const Skills = () => {
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div >
                     <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
-                    <p className='py-4'>// These are the technologies I've worked with.</p>
+                    <p className='py-4'>&frasl; &frasl; These are the technologies I've worked with.</p>
                 </div>
                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
                     <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
